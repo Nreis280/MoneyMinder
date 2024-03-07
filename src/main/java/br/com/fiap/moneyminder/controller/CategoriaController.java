74dda7d9b32dbae2a3997e114acd628cef821b11
Nreis280 <nicolasreissanto@gmail.com>
2024-03-07 17:47:15 -0300

    Criação dos mapeamentos DELETE e PUT

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -2,14 +2,17 @@ package br.com.fiap.moneyminder.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +42,7 @@ public class CategoriaController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Categoria> getById(@PathVariable Long id){
-       /*  for (Categoria categoria : repository){
+    /*  for (Categoria categoria : repository){
             if (categoria.id().equals(id)){
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
@@ -53,6 +56,47 @@ public class CategoriaController {
 
         return ResponseEntity.ok(optionalCategoria.get());
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("apagando categoria {}", id);
+
+        var optionalCategoria = buscarCategoriaPorId(id);
+
+        if (optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        repository.remove(optionalCategoria.get());
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Categoria categoria){
+        log.info("atualizando categoria id {} para {}", id, categoria);
+        
+        var optionalCategoria = buscarCategoriaPorId(id);
+
+        if (optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        var categoriaEncontrada = optionalCategoria.get();
+        var categoriaAtualizada = new Categoria(id, categoria.nome(), categoria.icone());
+        repository.remove(categoriaEncontrada);
+        repository.add(categoriaAtualizada);
+
+        return ResponseEntity.ok().body(categoriaAtualizada);
+    }
+
+    private Optional<Categoria> buscarCategoriaPorId(Long id) {
+        var optionalCategoria = repository
+                .stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+        return optionalCategoria;
+    }
+
 
     
 }
