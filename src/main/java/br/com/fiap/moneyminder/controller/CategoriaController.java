085cc234cc704c06ee0aecfbc3e0779a07a69ddf
Nreis280 <nicolasreissanto@gmail.com>
2024-03-08 09:39:11 -0300

    Refatoração e conecxao com banco de dados H2

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.moneyminder.model.Categoria;
+import br.com.fiap.moneyminder.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categoria")
@@ -26,6 +28,9 @@ public class CategoriaController {
     Logger log = LoggerFactory.getLogger(getClass());
 
     List<Categoria> repository = new ArrayList<>();
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
 
     @GetMapping
     public List<Categoria> index(){
@@ -40,62 +45,62 @@ public class CategoriaController {
         return ResponseEntity.status(HttpStatus.CREATED).body((categoria));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Categoria> getById(@PathVariable Long id){
-    /*  for (Categoria categoria : repository){
-            if (categoria.id().equals(id)){
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); */
+    // @GetMapping("/{id}")
+    // public ResponseEntity<Categoria> getById(@PathVariable Long id){
+    // /*  for (Categoria categoria : repository){
+    //         if (categoria.id().equals(id)){
+    //             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    //         }
+    //     }
+    //     return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); */
 
-        var optionalCategoria = repository.stream().filter((c)-> c.id().equals(id)).findFirst();
+    //     var optionalCategoria = repository.stream().filter((c)-> c.id().equals(id)).findFirst();
 
-        if (optionalCategoria.isEmpty())
-            return ResponseEntity.notFound().build();
+    //     if (optionalCategoria.isEmpty())
+    //         return ResponseEntity.notFound().build();
 
-        return ResponseEntity.ok(optionalCategoria.get());
-    }
+    //     return ResponseEntity.ok(optionalCategoria.get());
+    // }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<Object> destroy(@PathVariable Long id) {
-        log.info("apagando categoria {}", id);
+    // @DeleteMapping("{id}")
+    // public ResponseEntity<Object> destroy(@PathVariable Long id) {
+    //     log.info("apagando categoria {}", id);
 
-        var optionalCategoria = buscarCategoriaPorId(id);
+    //     var optionalCategoria = buscarCategoriaPorId(id);
 
-        if (optionalCategoria.isEmpty())
-            return ResponseEntity.notFound().build();
+    //     if (optionalCategoria.isEmpty())
+    //         return ResponseEntity.notFound().build();
 
-        repository.remove(optionalCategoria.get());
+    //     repository.remove(optionalCategoria.get());
 
-        return ResponseEntity.noContent().build();
+    //     return ResponseEntity.noContent().build();
 
-    }
+    // }
 
-    @PutMapping("{id}")
-    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Categoria categoria){
-        log.info("atualizando categoria id {} para {}", id, categoria);
+    // @PutMapping("{id}")
+    // public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Categoria categoria){
+    //     log.info("atualizando categoria id {} para {}", id, categoria);
         
-        var optionalCategoria = buscarCategoriaPorId(id);
+    //     var optionalCategoria = buscarCategoriaPorId(id);
 
-        if (optionalCategoria.isEmpty())
-            return ResponseEntity.notFound().build();
+    //     if (optionalCategoria.isEmpty())
+    //         return ResponseEntity.notFound().build();
 
-        var categoriaEncontrada = optionalCategoria.get();
-        var categoriaAtualizada = new Categoria(id, categoria.nome(), categoria.icone());
-        repository.remove(categoriaEncontrada);
-        repository.add(categoriaAtualizada);
+    //     var categoriaEncontrada = optionalCategoria.get();
+    //     var categoriaAtualizada = new Categoria(id, categoria.nome(), categoria.icone());
+    //     repository.remove(categoriaEncontrada);
+    //     repository.add(categoriaAtualizada);
 
-        return ResponseEntity.ok().body(categoriaAtualizada);
-    }
+    //     return ResponseEntity.ok().body(categoriaAtualizada);
+    // }
 
-    private Optional<Categoria> buscarCategoriaPorId(Long id) {
-        var optionalCategoria = repository
-                .stream()
-                .filter(c -> c.id().equals(id))
-                .findFirst();
-        return optionalCategoria;
-    }
+    // private Optional<Categoria> buscarCategoriaPorId(Long id) {
+    //     var optionalCategoria = repository
+    //             .stream()
+    //             .filter(c -> c.id().equals(id))
+    //             .findFirst();
+    //     return optionalCategoria;
+    // }
 
 
     
